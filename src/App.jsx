3d6ea621b238253e919f8ceb5681b44dbb8b199e
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-05 09:55:48 +0100

    project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-
+import { BrowserRouter } from 'react-router-dom'
+import {Hero, Navbar, About,Tech,Experience,Works,Feedbacks,Contact,EarthCanvas, BallCanvas, ComputersCanvas, 
+  StarsCanvas} from './components'
 const  App = () => {
 
   return (
-    <div>
-      3D developer portfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+
+        {/*............navbar.........*/}
+
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+
+        {/*............section.........*/}
+
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+
+        {/*............Contact.........*/}
+
+        <div className='relative z-0'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
