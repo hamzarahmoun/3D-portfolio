1e4be1e43ba376fdfbac3bac591448bf03e9e212
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-05 12:08:23 +0100

    navbare structure

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,7 +19,6 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -30,7 +28,7 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
-export {
+export const Assets = {
   logo,
   backend,
   creator,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Assets } from '../Assets/index.js'
+import { styles } from '../styles'
+import { Link } from 'react-router-dom'
+import { navLinks } from '../Constants/constants.js'; // Ensure you import navLinks
 
 const Navbar = () => {
+  const [active, setActive] = useState("'")
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link className='flex items-center gap-1' to='/' onClick={() => { setActive(""); window.scroll(0, 0) }}>
+          <img src={Assets.logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Adreian
+            <span className='sm:block hidden'> | javascript Mastery</span>  </p>
+        </Link>
+        <ul className='list-non hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
