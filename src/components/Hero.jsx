a3c6648b6a6424aaedec6bcbc08d9e4cfe1b8b04
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-09 22:23:48 +0100

    scroll bottom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { styles } from '../styles'
 import ComputersCanvas from './Canvas/Computers'
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -26,6 +27,24 @@ const Hero = () => {
       </div>
       {/*......... Canva ........*/}
       <ComputersCanvas />
+      {/*......... scroll bottom ........*/}
+      <div className='absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <motion.div 
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              animate={{y:[0,24,0]}}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              />
+            
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -32,7 +32,7 @@ const ComputersCanvas = () =>{
       <Suspense fallback={<Loader/>}>
       <OrbitControls
       enableZoom={false}
-      maxPolarAngle={Math.PI / 2}
+      maxPolarAngle={Math.PI / 2} 
       minPolarAngle={Math.PI / 2}
       />
         <Computers/>
