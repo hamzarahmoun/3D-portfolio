8b3d3c83881159a6a5d15ae2f4e69be7f366c25a
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-11 19:56:36 +0100

    T

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { services } from '../Constants/index'
 import { fadeIn, textVariant } from '../utils/motion'
 import { Tilt } from 'react-tilt'
-import { max } from 'three/webgpu'
+// import SectionWrapper from '../hoc/SectionWrapper'
 const About = () => {
   const ServiceCard = ({ index, title, icon }) => {
     return (
@@ -65,5 +65,7 @@ const About = () => {
     </>
   )
 }
-
 export default About
+
+// export default SectionWrapper
+// {About, "about"}
---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import {styles} from '../styles'
+// import {motion} from 'framer-motion'
+// import { staggerContainer } from '../utils/motion'
+// const SectionWrapper = (Component,idName) => 
+//     function HOC() {
+//     return (
+//         <motion.section 
+//         variants={staggerContainer()}
+//         initial='hidden'
+//         whileInView='show'
+//         viewport={`${styles.paddingY} max-w-7xl mx-auto relative-z-0` }
+//         >
+//             <Component /> 
+//         </motion.section>
+//     )
+// }
+
+
+// export default SectionWrapper
