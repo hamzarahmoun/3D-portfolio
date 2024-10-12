d9f552ec1f9651ec737737937e1319a298a884b6
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-12 21:28:28 +0100

    expereince head and sub text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { VerticalTimeline } from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
+import 'react-vertical-timeline-component/style.min.css'
+import { styles } from '../styles'
+import { experiences } from '../Constants'
+import SectionWrapper from '../Hoc/SectionWrapper'
+import { textVariant } from '../utils/motion'
 const Experience = () => {
   return (
-    <div></div>
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText} >What i have done so far</p>
+        <p className={styles.sectionHeadText}>Work Experience</p>
+      </motion.div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work")
