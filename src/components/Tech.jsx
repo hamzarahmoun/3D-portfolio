8438809dbf3857f9da7f675d2104b28d0d8802ad
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-14 15:51:44 +0100

    Tech structure

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {BallCanvas} from './Canvas'
+import SectionWrapper from '../Hoc/SectionWrapper'
+import {technologies} from '../Constants'
+import { div } from 'framer-motion/client'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology)=>(
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper ( Tech,'tech')
