aecf3e8882eba96931f1e855706eeea1962ba893
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-15 08:12:56 +0100

    project section headline text

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import {github} from '../Assets'
+import {projects} from '../Constants'
+import SectionWrapper from '../hoc/SectionWrapper'
+import { textVariant } from '../utils/motion'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    
+      {/*......... AboutHeadline + AboutSubHeadline ........*/}
+
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Works</p>
+        <h2 className={styles.heroHeadText}>Projects.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works,"")
