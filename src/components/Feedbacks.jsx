300e53c712193ba7797dc8bf23f7486f4f02d036
Hamza Rahmoun <hamza.rahmoun456@gmail.com>
2024-10-30 13:36:58 +0100

    Update Feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import SectionWrapper from '../Hoc/SectionWrapper'
+import SectionWrapper from '../hoc/SectionWrapper'
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
