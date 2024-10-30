5cd3fec232e6d167f877294f5a08e7a0bab587a5
Hamza Rahmoun <hamza.rahmoun456@gmail.com>
2024-10-30 13:39:44 +0100

    Update Feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import SectionWrapper from '../hoc/SectionWrapper'
 import { fadeIn, textVariant } from "../utils/motion";
-import { testimonials } from "../constants";
+import { testimonials } from "../Constants";
 
 const Feedbacks = () => {
   const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => {
