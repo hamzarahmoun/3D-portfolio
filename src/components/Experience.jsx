9a574c20e079a537823b407459322565e90cc15a
Hamza Rahmoun <hamza.rahmoun456@gmail.com>
2024-10-30 13:40:12 +0100

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 import { styles } from '../styles'
 import { experiences } from '../Constants'
-import SectionWrapper from '../Hoc/SectionWrapper'
+import SectionWrapper from '../hoc/SectionWrapper'
 import { textVariant } from '../utils/motion'
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
