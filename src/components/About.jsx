5bfc92991ccae7037eb43740e6abb4dac32fd633
Hamza Rahmoun <hamza.rahmoun456@gmail.com>
2024-10-30 13:40:47 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { services } from '../Constants/index'
 import { fadeIn, textVariant } from '../utils/motion'
 import { Tilt } from 'react-tilt'
-import SectionWrapper from '../Hoc/SectionWrapper'
+import SectionWrapper from '../hoc/SectionWrapper'
 const About = () => {
   const ServiceCard = ({ index, title, icon }) => {
     return (
