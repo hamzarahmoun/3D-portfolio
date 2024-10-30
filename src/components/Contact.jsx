c9c87a48158894789142acdf093fe217dc56b1ef
Hamza Rahmoun <hamza.rahmoun456@gmail.com>
2024-10-30 13:40:30 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
-import SectionWrapper from '../Hoc/SectionWrapper'
+import SectionWrapper from '../hoc/SectionWrapper'
 
 import { slideIn } from "../utils/motion";
 const Contact = () => {
